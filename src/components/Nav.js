10ae8afa310ecdfb61c8baf95bc389207d5aec5b
Karan Mann <karan.mann@outlook.com>
2022-01-13 11:27:56 +0100

    Errors Fixed and Audio Volume fixed

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,22 +2,41 @@ import { Link } from "react-router-dom";
 
 import navLogo from "../assets/Sveriges Radio logo 2011.png";
 
-import clickSound from '../assets/click.wav'
+import clickSound from "../assets/click.wav";
 
-let audio = new Audio(clickSound)
+let audio = new Audio(clickSound);
+audio.volume = 0.3;
 
 const playSound = () => {
-  audio.play()
-}
+  audio.play();
+};
 
 export const Nav = () => {
   return (
     <div className="nav-container">
       <img className="nav-image" src={navLogo} alt="navlogo" />
       <div className="nav-link-container">
-        <Link className="nav-link" to="/Programs" onClick={() => playSound()}>Programs</Link>
-        <Link className="nav-link" to="/radiochannels" onClick={() => playSound()}>Radio Channels</Link>
-        <Link className="nav-link" to="/weather" onClick={() => playSound()}>Weather</Link>
+        <Link 
+          className="nav-link" 
+          to="/Programs" 
+          onClick={() => playSound()}
+        >
+          Programs
+        </Link>
+        <Link
+          className="nav-link"
+          to="/radiochannels"
+          onClick={() => playSound()}
+        >
+          Radio Channels
+        </Link>
+        <Link 
+          className="nav-link" 
+          to="/weather" 
+          onClick={() => playSound()}
+        >
+          Weather
+        </Link>
       </div>
     </div>
   );
